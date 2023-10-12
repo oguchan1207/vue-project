043bc3b7de6d3da6de4444647970582bb5a38064
oguchan1207 <oguchan1207@gmail.com>
2023-10-12 11:20:15 +0900

    vue_fristCommit

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+  import MyPractice from "./components/MyPractice.vue"
+</script>
+
+<template>
+  <main>
+    <h1>Vue.jsの練習</h1>
+    <MyPractice
+      initialMessage="ボタンを押してください。"
+      clickedMessage="ボタンが押されました！"
+      buttonLabel="ボタン"
+    />
+    <MyPractice
+      initialMessage="スイッチを押して！"
+      clickedMessage="スイッチが押された！"
+      buttonLabel="スイッチ"
+      />
+  </main>
+</template>
+
+<style scoped>
+  h1 {
+    color: red;
+  }
+</style>
